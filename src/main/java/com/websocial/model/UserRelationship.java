0be28d2dc379267dadb72e12b89a67b2a1df45ec
LocTM <tranminhloc.anhk@gmail.com>
2024-03-21 11:24:42 +0700

    add table relationship

--- src/main/java/com/websocial/model/UserRelationship.java
+++ src/main/java/com/websocial/model/UserRelationship.java
@@ -8,17 +8,18 @@ import java.time.LocalDate;
 
 @Entity
 @Data
-public class UserRelationShip {
+@Table (name = "user_relationship")
+public class UserRelationship {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @CreationTimestamp
     private LocalDate createDate;
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id_1", insertable = false, updatable = false)
     private User user1;
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id_2", insertable = false, updatable = false)
     private User user2;
     private String status;
 }
