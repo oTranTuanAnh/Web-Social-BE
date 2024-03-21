0a8408be2cc82c69b7ce5d436cdd5b64f12e99b9
tuan anh <tuananhtran.xd@gmail.com>
2024-03-21 09:18:40 +0700

    register done

--- src/main/java/com/websocial/repo/AppRoleRepo.java
+++ src/main/java/com/websocial/repo/AppRoleRepo.java
@@ -5,6 +5,11 @@ import com.websocial.model.appUser.AppRole;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 @Repository
 public interface AppRoleRepo extends CrudRepository<AppRole, Long> {
+    AppRole findOneByName(String name);
+    Set<AppRole> findAllByIdIn(List<Long> ids);
 }
